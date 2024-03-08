f53066834b20edb8e3cb06e075290a363d384f6a
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-03-07 19:09:52 -0800

    fun stats test on bipartiteness

--- test.py
+++ test.py
@@ -2,6 +2,26 @@ from GraphLib import *
 from Tree import *
 from Graph import *
 
-graph = createSimpleGraph(5, 1)
-print(graph.toString())
+values = []
+
+for i in range(1000):
+    graph = createSimpleGraph(10, 2)
+    count = 0
+
+    while not graph.isBipartite():
+        graph = createSimpleGraph(10, 2)
+        count += 1
+
+    values.append(count)
+    count = 0
+
+total = 0
+for num in values:
+    total += num
+
+print(f"mean = {total/1000}")
+print(f"meadian = {(values[500] + values[499])/2}")
+
+
+
 
